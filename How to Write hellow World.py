620df54a4cf711c058634febb760584986c08711
torabutt <57006036+torabutt@users.noreply.github.com>
2020-05-23 01:04:23 +0530

    First Day

---
+++ How to Write hellow World.py
@@ -0,0 +1,44 @@
+# This is famous first question for any programming language . The question is print -> Hello World!.
+
+# one way
+
+print('Hello World!') # so easy
+
+#second way
+
+print("Hello World!") # this will work fine for any statement .. but not above one.. read full to know
+
+# third way
+
+print('Hello'+' '+'World!') # it is like string concatenation
+
+# Forth way
+
+print('Hello',end=' ') # the next print statement will be start printing in the same line
+
+print('World!')  # if no end is provided then new print statement will start printing in new line. 
+
+# end will help you keep track of last line and it will end up with the ->' '
+
+# Fifth Way 
+
+print('Hello','World!')
+
+# You might be thinking that why single single->' ' and double ->" " both are used . 
+
+# The answer is both can be used unless we required to write sentence like -> I don't hurt animal. In ->(don't) we use single
+
+# -> (') so this get touble in the print statement 
+
+# eg
+print('i don't hurt animal') # this will give you the error. 
+
+# so we can use ->(") 
+
+#eg
+print("i don't hurt animal") # this will work fine. 
+
+# congratulation . You Have learn the first basic question . 
+
+# see you soon
+
