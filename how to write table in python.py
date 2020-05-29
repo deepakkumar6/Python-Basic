c90210c69ed2ca739b5a4efc0b662b3788349e59
torabutt <57006036+torabutt@users.noreply.github.com>
2020-05-30 01:30:04 +0530

    table table

---
+++ how to write table in python.py
@@ -0,0 +1,43 @@
+# taking input from the user
+
+n = input()
+
+# you want to print the table of given interger
+
+for i in range(1,11):
+  print(count*i)
+ 
+# the given command will allow us to print the table of number n
+
+#formate will be like this
+
+#n
+#2n
+#3n
+#4n
+#5n
+#6n
+#7n
+#8n
+#9n
+#10n
+
+# if you want to print the table in a single line
+
+for i in range(1,11):
+  print(n*i,end=' ')
+
+# formate of the above code will be like this..
+
+#n 2n 3n and so on
+
+# we can also change the way we want to print
+
+for i in range(1,11):
+  print(n,'*',i,'=',n*i)
+
+# this will be like good like little kid table
+
+# i see some of contest also focus on what you want to print and how you can print it...
+
+That it for today... see you soon
